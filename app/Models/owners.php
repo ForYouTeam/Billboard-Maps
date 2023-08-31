58d53ea770133c9b948692f8a0a5edf14ffa88c2
IPKIN9 <irwandirini@gmail.com>
2023-09-01 06:42:24 +0700

    Create: model and migration

---
+++ app/Models/owners.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class owners extends Model
+{
+    use HasFactory;
+    protected $table = 'owners';
+    protected $fillable = [
+        'id', 'name', 'company_name', 'email', 'address', 'phone', 'user_id',
+        'created_at', 'updated_at'
+    ];
+}
